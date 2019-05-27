f2b4904ed5188a5def8cbd788917dc130688a218
zhanglei <zhanglei@inesadt.com>
2019-05-27 16:26:56 +0800

    初始化

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="wrapper">
+    <h1>bbq</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing-page',
+  components: {},
+  methods: {}
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+</style>
